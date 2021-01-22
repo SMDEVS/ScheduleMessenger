cd5acf1fd0a4d0184c7feb83defc7740f8467fc3
neha-b2001 <binwal.neha@gmail.com>
2021-01-23 01:13:47 +0530

    feat(ScheduleMessenger): implement cancellation of scheduled messages
    - implemented cancellation of already scheduled messages (HistoryFragment.java)
    - also implemented deletion of scheduled message cards in RecyclerView, keeping scheduled alarm intact
    - made use of xabaras/RecyclerViewSwipeDecorator to add swipe decor to RecyclerView items

--- app/src/main/java/com/example/schedulemessenger/View/Adapter/MessageAdapter.java
+++ app/src/main/java/com/example/schedulemessenger/View/Adapter/MessageAdapter.java
@@ -3,6 +3,7 @@ package com.example.schedulemessenger.View.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -34,14 +35,48 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageH
     @Override
     public void onBindViewHolder(@NonNull MessageHolder holder, int position) {
         Message currentMessage = messageList.get(position);
+
+        int type = currentMessage.getMessageType();
+        int imageResourceId = -1;
+        if(type == 1) {
+            imageResourceId = R.drawable.ic_sms;
+        } else if(type == 2) {
+            imageResourceId = R.drawable.ic_whatsapp;
+        } else if(type == 3) {
+            imageResourceId = R.drawable.ic_email;
+        } else if(type == 4) {
+            imageResourceId = R.drawable.ic_instagram;
+        }
+
+        /* Formatting timeString in a more presentable manner */
+        String timeString = currentMessage.getTimeString().trim();
+        int indexOfSlash = timeString.indexOf('/');
+        int indexOfSpace = timeString.indexOf(' ');
+        int indexOfColon = timeString.indexOf(':');
+        String month = timeString.substring(0, indexOfSlash);
+        String day = timeString.substring(indexOfSlash + 1, indexOfSlash + 3);
+        String year = timeString.substring(indexOfSlash + 4, indexOfSpace);
+        String time = timeString.substring(indexOfSpace + 1, indexOfColon + 3);
+        if(timeString.endsWith("pm")) {
+            time = (Integer.parseInt(time.substring(0, 2)) + 12) + "" + time.substring(2);
+        }
+        timeString = day + '/' + month + "/" + year + " " + time;
+
+        if(type != -1) {
+            holder.imageView.setImageResource(imageResourceId);
+        }
         holder.phoneNumber.setText(currentMessage.getPhoneNumber());
-        holder.messageStatus.setText(currentMessage.getMessageStatus());
+        holder.messageStatus.setText(timeString);
         holder.messageText.setText(currentMessage.getMessageText());
     }
 
     @Override
     public int getItemCount() {
         return messageList.size();
+    }
+
+    public Message getMessageAt(int position) {
+        return messageList.get(position);
     }
 
     public void setMessageList(List<Message> messageList) {
@@ -53,9 +88,11 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageH
         public TextView phoneNumber;
         public TextView messageText;
         public TextView messageStatus;
+        public ImageView imageView;
 
         public MessageHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.image_view);
             phoneNumber = itemView.findViewById(R.id.phone_number_text_view);
             messageText = itemView.findViewById(R.id.message_content_text_view);
             messageStatus = itemView.findViewById(R.id.message_status_text_view);
